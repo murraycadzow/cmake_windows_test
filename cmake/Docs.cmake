001be72f3390e774f69e4b59cc3acb86e61b7f12
murraycadzow <cadmu661@student.otago.ac.nz>
2024-08-19 14:23:00 +1200

    set up doxygen

---
+++ cmake/Docs.cmake
@@ -0,0 +1,13 @@
+option(BUILD_DOC "Build documentation" ON)
+find_package(Doxygen)
+
+if (DOXYGEN_FOUND)
+    add_custom_target(docs
+        ${DOXYGEN_EXECUTABLE}
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/docs/
+        COMMENT "Generating HTML documentation with Doxygen"
+    )
+    message("Directory " ${CMAKE_SOURCE_DIR}/docs/)
+    else (DOXYGEN_FOUND)
+    message("Doxygen not found")
+endif (DOXYGEN_FOUND)
